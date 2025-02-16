673e74554676ff2c2ba4fcd3fdba557bd4163e5f
p-med <pmedinaavalos323@gmail.com>
2025-02-16 11:20:32 -0500

    Added initial EDA script.

---
+++ EDA.R
@@ -0,0 +1,72 @@
+#Initial script
+
+#Load libraries
+
+library(tidyverse)
+library(here)
+library(sf)
+
+#Load data
+
+if(!dir.exists("./data")){dir.create(here("./data"))}#Set-up directory for work and data
+
+#Download data
+
+url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
+temp <- tempfile() #Temporary variable to save zip file
+download.file(url,temp)
+
+#Export data
+
+#unzip(temp, list = TRUE) #Explore zip folder
+#zip_list <- as.vector(unzip(temp, list = TRUE)[,1]) #Save structure
+
+repdata <- read.table(temp, sep = ",", header = TRUE)
+
+unlink(temp); rm(temp, url) #Delete temporary files
+
+str(repdata)
+
+#Across the United States, which types of events (as indicated 
+#in the EVTYPEEVTYPE variable) are most harmful with respect to population health?
+
+eventype <- repdata %>% 
+        group_by(EVTYPE) %>%
+        summarise(fatalities = sum(FATALITIES), 
+                  mag = sum(MAG),
+                  injuries = sum(INJURIES),
+                  propmg = sum(PROPDMG))
+
+fatalities <- eventype %>% filter(fatalities != 0)
+
+fatalities %>% 
+        top_n(20, fatalities) %>%
+        ggplot() + 
+        geom_bar(aes(x=fatalities, 
+                     y=reorder(EVTYPE,+fatalities)),
+                 stat="identity") +
+        labs(title="Fatalities",x="",y="")
+        
+injuries <- eventype %>% filter(injuries != 0)
+        
+injuries %>% 
+        top_n(20, injuries) %>%
+        ggplot() + 
+        geom_bar(aes(x=injuries, 
+                     y=reorder(EVTYPE,+injuries)),
+                 stat="identity") +
+        labs(title="Injuries",x="",y="")
+
+#Across the United States, which types of events have the greatest economic consequences?
+
+propmg <- eventype %>% filter(propmg != 0)
+
+propmg %>% 
+        top_n(20, propmg) %>%
+        ggplot() + 
+        geom_bar(aes(x=propmg, 
+                     y=reorder(EVTYPE,+propmg)),
+                 stat="identity") +
+        labs(title="Property Damage",x="",y="")
+
+        
